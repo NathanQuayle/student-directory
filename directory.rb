5cbee15d141d72e67806ef1ce44c35f309167827
Nathan Quayle <nathan.quayle@gmail.com>
2018-08-24 13:29:34 +0000

    Updated directory.rb to be interactive.

--- directory.rb
+++ directory.rb
@@ -1,63 +1,131 @@
-# students = [
-#   {name: "Dr. Hannibal Lecter", cohort: :november},
-#   {name: "Darth Vader", cohort: :november},
-#   {name: "Nurse Ratched", cohort: :november},
-#   {name: "Michael Corleone", cohort: :november},
-#   {name: "Alex DeLarge", cohort: :november},
-#   {name: "The Wicked Witch of the West", cohort: :november},
-#   {name: "Terminator", cohort: :november},
-#   {name: "Freddy Krueger", cohort: :november},
-#   {name: "The Joker", cohort: :november},
-#   {name: "Joffrey Baratheon", cohort: :november},
-#   {name: "Norman Bates", cohort: :november}
-# ]
+@students = []
 
 def print_header
   
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(100)
+  puts "-------------".center(100)
   
 end
 
-def print(students)
+def print_students_list
   
-  students.each do |student|
-    
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  sorted = {}
+  
+  # Sort by cohort.
+  @students.each do |student|
+   
+    if !sorted.key?(student[:cohort])
+      sorted[student[:cohort]] = [student[:name]]
+    else
+      sorted[student[:cohort]].push(student[:name])
+    end
     
   end
   
+  sorted.each do |k, v|
+    
+    puts k.to_s.center(100)
+    v.each_with_index { |e, i| puts "#{i + 1}. #{e}".center(100) }
+    
+  end
+
 end
 
-def print_footer(names)
+def print_footer
   
-  puts "Overall, we have #{names.count} great students"
+  if @students.count == 0
+    puts "We have no students".center(100) if @students.count == 0
+  else
+    puts "Overall, we have #{@students.count} great students".center(100) 
+  end
   
 end
 
 def input_students
   
-  puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
-  students = []
-  # get first name
-  name = gets.chomp
-  
-  while !name.empty? do
+
+  loop do
     
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    puts "Please enter the names of the students"
+    name = gets.chomp.capitalize.to_sym
+    break if name == :""
+    puts "Enter a cohort"
+    cohort = gets.chomp.capitalize.to_sym
+    cohort = :"September" if cohort == :""
+    @students << {
+    name: name, 
+    cohort: cohort, 
+    }
     
-    name = gets.chomp
+    if @students.count == 1
+      puts "Now we have 1 student"
+    else
+      puts "Now we have #{@students.count} students"
+    end
     
   end
   
-  students
+end
+
+def interactive_menu 
+  loop do
+    
+    print_menu
+    process(gets.chomp)
+	
+  end
   
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def print_menu 
+
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Save students."
+    puts "9. Exit" 
+  
+end
+
+def show_students
+  
+  print_header
+  print_students_list
+  print_footer
+  
+end
+
+def process(selection)
+  
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again."
+  end
+  
+end
+
+def save_students
+  
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  #itierate over the array of students
+  @students.each do |student|
+    
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+    
+  end
+  file.close
+end
+
+interactive_menu
